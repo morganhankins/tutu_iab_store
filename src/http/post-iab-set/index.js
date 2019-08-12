0406cc6eb59af485f2dc737bc38ba618b8c430c7
Morgan Hankins <morganhankins@gmail.com>
2019-08-12 15:08:38 -0700

    first

--- src/http/post-iab-set/index.js
+++ src/http/post-iab-set/index.js
@@ -3,10 +3,26 @@
 //
 // let begin = require('@architect/functions')
 
+let data = require('@begin/data')
+
 exports.handler = async function http(req) {
   console.log(req)
-  return {
-    status: 302,
-    location: '/'
-  }
+
+    let key = req.body['key']
+    let token = await data.get({'iab', key})
+
+    if(token == null) {
+	// fail token already exits
+	return {
+	    status: 401,
+	    location: '/iab/set'
+	}
+    } else {
+	await data.set({table, key, "y"}) // TODO :)
+	
+	return {
+	    status: 200,
+	    location: '/iab/set'
+	}
+    }
 }
